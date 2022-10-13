37ed39d6c6c090befbfeee8dd55f18e6a445e221
2021BTech <daffydanni@gmail.com>
2022-10-13 11:34:14 +0100

    Topbar done

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+// css import
 import "./Topbar.css";
+// Icons import
+import {NotificationsNone, Settings} from '@material-ui/icons';
+//img
+import Avt from '../../assets/img/male.png';
 
 const Topbar = () => {
   return (
     <div className='topbar'> 
         <div className='topbarWrapper'>
-            <div topbarLeft>
+            <div className='topbarLeft'>
                 <span className='logo'> HSE ADMIN-END</span>
             </div>
-            <div topbarRight> Right </div>
+            <div className='topbarRight'>
+                <div className='topbarIcon'>
+                    <NotificationsNone />
+                    <span className='topIconBadge'>8</span>
+                </div>
+                <div className='topbarIcon'>
+                    <Settings />
+                </div>
+                <img src={Avt} alt='' className='topAvatar' />
+            </div>
         </div>
-      
     </div>
   )
 }
